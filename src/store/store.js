b5b09cb4d392211b616939cfabe50470dffe72f9
Julia Gao Miller <julia.gao.miller@gmail.com>
2017-12-30 19:47:58 -0800

    using mutations: methods from store.js

--- src/store/store.js
+++ src/store/store.js
@@ -18,5 +18,13 @@ export const store = new Vuex.Store({
     multipleCounter: state => {
       return state.counter * state.multiple
     }
+  },
+  mutations: {
+    increment: state => {
+      state.counter++
+    },
+    decrement: state => {
+      state.counter--
+    }
   }
 });
